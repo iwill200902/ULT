0eeaca03f77d1d6027de73a9baa4c963152373d1
iwill <iwill200902@gmail.com>
2026-04-06 17:06:25 +0900

    arrange files

--- Library-study/Hello-Lib/build/CMakeFiles/main.dir/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/main.dir/main.cpp.o: \
- /home/motoki/ULT-study/Library-study/Hello-Lib/main.cpp \
- /usr/include/stdc-predef.h \
- /home/motoki/ULT-study/Library-study/Hello-Lib/hello/hello.hpp
