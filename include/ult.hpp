4dd7c146eed1c7c6871e3ee06c34a8e6829448fe
iwill <iwill200902@gmail.com>
2026-05-03 00:14:01 +0900

    [add] add queue

--- include/ult.hpp
+++ include/ult.hpp
@@ -1,8 +1,10 @@
 #pragma once
 #include "ult/thread.hpp"
 #include <array>
+#include <queue>
 
 constexpr std::size_t MAX_THREAD = 4096;
 extern std::array<TCB, MAX_THREAD> tcb_table;
+extern std::queue<TCB*> tcb_queue;
 
 extern int64_t uthread(void (*fanc)());
