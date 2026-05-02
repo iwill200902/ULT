e4458ceb267b617bfc6aba0b6c3c85fe6c6ad347
iwill <iwill200902@gmail.com>
2026-05-03 02:12:25 +0900

    [refactor] move queue and array definitions to ult.cpp

--- include/ult.hpp
+++ include/ult.hpp
@@ -1,11 +1,5 @@
 #pragma once
 #include "ult/thread.hpp"
-#include <array>
-#include <queue>
-
-constexpr std::size_t MAX_THREAD = 4096;
-extern std::array<TCB, MAX_THREAD> tcb_table;
-extern std::queue<TCB*> tcb_queue;
 
 extern int64_t uthread(void (*fanc)());
 extern int64_t register_main();
